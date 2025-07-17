701afe1e93527a74115d312ee698c401d9b906e1
Pierre Z. <lespoyler31@gmail.com>
2025-07-17 12:23:41 +0100

    Update send_mail.py - Delete unuseful comments

--- src/send_mail.py
+++ src/send_mail.py
@@ -1,12 +1,5 @@
 #coding = utf-8
 
-"""
-- OS error: CPU or RAM
-- New type of log detected for an update
-- Unautorised authentification
-- More than 3 times wrong authentification
-"""
-
 import base64
 from email.mime.text import MIMEText
 from googleapiclient.discovery import build
